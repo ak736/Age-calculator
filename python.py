c66508e76726d58b08aa4c6f24dcaa3c00212671
ak736 <95230817+ak736@users.noreply.github.com>
2022-03-04 21:04:22 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?\n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+#1 year = 365 days
+#1 year = 52 weeks
+#1 year = 12 months
+new_age = int(age)
+numberofyearsleft = 90 - new_age
+numberofdaysleft = numberofyearsleft * 365
+numberofweeksleft = numberofyearsleft * 52
+numberofmonthsleft = numberofyearsleft * 12
+print(f"You have {numberofdaysleft} days left, You have {numberofweeksleft} weeks left, You have {numberofmonthsleft} months left")
+
+
+
+
+
+
+
+
+
